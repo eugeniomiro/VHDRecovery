405385671420be77df8d678b567fc1df2d371007
Eugenio Miró <eugenio.miro@gmail.com>
2014-09-04 07:01:40 -0300

    using cleanup

--- VHDHdr/CmdLineParams.cs
+++ VHDHdr/CmdLineParams.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using CommandLine;
 using CommandLine.Text;
 
@@ -11,14 +7,14 @@ namespace VHDHdr
     class CmdLineParams
     {
         [Option('f', "filename", Required = true)]
-        public String FileName { get; set; }
+        public  String  FileName    { get; set; }
+        [Option('o', "fixfooter", Required = false)]
+        public  bool    FixFooter   { get; set; }
         [HelpOption]
         public string GetUsage()
         {
             return HelpText.AutoBuild(this,
               (HelpText current) => HelpText.DefaultParsingErrorsHandler(this, current));
         }
-        [Option('o', "fixfooter", Required = false)]
-        public bool FixFooter { get; set; }
     }
 }
